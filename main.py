7855eae08d0a08144f4be9eb83c8e456ab7dfe69
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-06 20:54:45 -0300

    Add the 'main.py' file

---
+++ main.py
@@ -0,0 +1,10 @@
+from converter.ascii_converter import ASCIIConverter
+from PIL import Image
+
+
+original_image_name = 'mises'
+
+image = Image.open(f'./{original_image_name}.jpg')
+
+converter = ASCIIConverter()
+new_image = converter.convert_to_ascii(image, f'./{original_image_name}.txt', 2000)
