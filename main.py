a71a2b1a7de7727a5b9df8fcf03b600f7f75b4c8
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-11 10:44:53 -0300

    Update main.py to change on convert_to_ascii() method

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from converter.ascii_converter import ASCIIConverter
+from converter.image_saver import ASCIIImageSaver
 from PIL import Image
 
 
-original_image_name = 'rothbard'
+original_image_name = 'knife'
 
-image = Image.open(f'./{original_image_name}.jpg')
+image = Image.open(f'./examples/{original_image_name}.jpg')
 
 converter = ASCIIConverter()
-new_image = converter.convert_to_ascii(image, f'./{original_image_name}.txt', 200)
+new_image = converter.convert_to_ascii(image)
+
+saver = ASCIIImageSaver()
+saver.save_image(new_image, f'./examples/{original_image_name}.txt')
