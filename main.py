4868b6070702cec044652dd5d768c617dc26479a
Shubhendra Kushwaha <shubhendrakushwaha94@gmail.com>
2020-12-27 11:50:46 +0530

    Add command line argument handler to get inpul file path and image width

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ from converter.ascii_converter import ASCIIConverter
 from converter.image_saver import ASCIIImageSaver
 from converter.image_resizer import ImageResizer
 from PIL import Image
+import sys
 
+default_image_file = 'examples/snake.png'
+if len(sys.argv)==1:
+  image_file=default_image_file
+elif len(sys.argv)==2:
+  image_file = sys.argv[1]
+  image_width = 100
+elif len(sys.argv)>2:
+  image_file = sys.argv[1]
+  image_width = int(sys.argv[2])
+  
+image = Image.open(image_file)
 
-
-image = Image.open('examples/snake.png')
+resizer = ImageResizer()
+resized_image = resizer.resize_image(image,image_width)
 
 converter = ASCIIConverter()
-resizer = ImageResizer()
-resized_image = resizer.resize_image(image)
 new_image = converter.convert_to_ascii(resized_image)
 
 saver = ASCIIImageSaver()
