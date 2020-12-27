8c6d174760f57131f8a31ac1e54a8cf5ea92b774
Gustavo Azevedo Naldoni <gunaldoni@gmail.com>
2020-12-27 11:08:59 -0300

    Take off the resize image function because it's already on ASCIIConverter class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from converter.ascii_converter import ASCIIConverter
 from converter.image_saver import ASCIIImageSaver
-from converter.image_resizer import ImageResizer
 from PIL import Image
 import sys
 
@@ -16,11 +15,8 @@ elif len(sys.argv)>2:
   
 image = Image.open(image_file)
 
-resizer = ImageResizer()
-resized_image = resizer.resize_image(image,image_width)
-
 converter = ASCIIConverter()
-new_image = converter.convert_to_ascii(resized_image)
+new_image = converter.convert_to_ascii(image)
 
 saver = ASCIIImageSaver()
-saver.save_image(new_image, 'snake.txt')
+saver.save_image(new_image, './examples/snake.txt')
